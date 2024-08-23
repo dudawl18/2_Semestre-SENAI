9e913f18f339e136cfa0c49a47ef68953ee31389
dudawl18 <maria_ew_luz@estudante.sesisenai.org.br>
2024-08-23 17:16:25 -0300

    Criação do primeiro projeto com React!!!

---
+++ oiReact/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+         <h1>Dinossauros</h1>
+         <h2>Carnívoros & Herbívoros</h2>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit ducimus dolore ipsum. Laborum tempore, officiis cum consequuntur quae sit laudantium! Quidem sapiente temporibus voluptatibus. Deserunt architecto iusto repudiandae vero reiciendis?</p>
+
+    </>
+  )
+}
+
+export default App
