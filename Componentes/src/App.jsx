a2358d1e6a9d40ba23b166eede8e68aeb1f6f5f2
dudawl18 <maria_ew_luz@estudante.sesisenai.org.br>
2024-08-29 17:20:57 -0300

    add criação de componentes e mais umas coisas

---
+++ Componentes/src/App.jsx
@@ -0,0 +1,36 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+       <h1>Atalhos do VS Code</h1>
+       <h2>Coisas para facilitar a tua vida</h2>
+       <p>
+        Comando para o emmet da ul: ul>li*6   {/*cria uma lista com 6 topicos nela */}
+       </p>
+        <ul>
+          <li>Atalho: alt + z para quebra automática de linha</li>
+          <li>Atalho: alt + clique esquerdo para adicionar novos cursores na tela</li>
+          <li>Atalho: ctrl + shift + k para excluir uma linha inteira</li>
+          <li>Atalho: ctrl + ; para comentar/descomentar</li>
+          <li>Atalho: windows + . abre teclado de emojis</li>
+          <li>Atalho: alt + ↕ para mover uma linha ou um bloco</li>
+          <li>Atalho: alt + shift + ↕ copia a linha atual ou bloco selecionado</li>
+          <li>Atalho: ctrl + j ou ctrl + ' abre/fecha o terminal</li>
+        </ul>
+        <p>Um comando parecido com control + D:  Prescionar a rodinha do mouse e clicar no tela e mover o cursor</p>
+
+        <p>Extensões: </p>
+        <ol>
+          <li>ES7 React/Redux/GraphQL...</li>
+          <li>Auto Rename Tag</li>
+          <li>Color Highlight</li>
+          <li>Power mode</li>
+          <li>VS Code Pets</li>
+        </ol>
+    </>
+  )
+}
+
+export default App
