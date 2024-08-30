2c0f0c73702c7660cfb898fa92ebb2cfc66f3abb
dudawl18 <maria_ew_luz@estudante.sesisenai.org.br>
2024-08-30 17:10:44 -0300

    Aprender a classificar as cores no css (cor-1)e usa-las, ao inves dos nomes hexadecimais

--- componenteDeVerdade/src/components/Cabecalho.jsx
+++ componenteDeVerdade/src/components/Cabecalho.jsx
@@ -3,9 +3,30 @@ import './Cabecalho.css'
 function Cabecalho() {
   return (
     <div className="Cabecalho-container">
-       <h1>Treinar dragão</h1>
+       <h1>Gravity Falls</h1>
+       <img src="./images/gravity-falls-mystery.gif" alt="" />
     </div>
   )
 }
 
 export default Cabecalho
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ componenteDeVerdade/src/components/Corpo.jsx
@@ -0,0 +1,10 @@
+
+function Corpo() {
+    return (
+       <div>
+            <h3>Melhor animação ☝</h3>
+       </div>
+    )
+  }
+  
+  export default Corpo
