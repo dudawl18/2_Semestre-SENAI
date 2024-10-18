e4b0161eba066cf02a778fe356c4c2f375187f39
MARIA EDUARDA WOLF LUZ <maria_ew_luz@estudante.sesisenai.org.br>
2024-10-18 17:13:41 -0300

    Realizei a atividade 7.15

--- exercicio_(RouterContext)/src/pages/Numero715.jsx
+++ exercicio_(RouterContext)/src/pages/Numero715.jsx
@@ -1,32 +1,31 @@
-import React, { useState } from 'react'
-import Navbar from '../components/Navbar'
+import React, { useState } from 'react';
+import Navbar from '../components/Navbar';
 
 function Numero715() {
-    const [inputNumero, setInputNumero] = useState()
-    const [verificacao, setVerificacao] = useState()
-    const [mostrarResultado, setMostrarResultado] = useState(false)
+    const [inputNumero, setInputNumero] = useState('')
+    const [listaNuns, setListaNuns] = useState([])
+    const [result, setResult] = useState(null)
+    const [controle, setControle] = useState(false)
 
     function verificarNumeros() {
-        let numeroDigitado = parseInt(inputNumero)
-        let maiorNumDigitado = 0
+        const numero = parseInt(inputNumero, 10)
 
-        if (numeroDigitado === -1) {
-            setMostrarResultado(true)
-            return
-        }
+        if (numero > 0) {
+            setListaNuns([...listaNuns, numero]) // Adiciona o número na listaNuns
 
-        if (numeroDigitado > 0) {
-            if (numeroDigitado > maiorNumDigitado) {
-                numeroDigitado = maiorNumDigitado
-                setVerificacao(numeroDigitado)
+            if (listaNuns.length === 0 || numero > Math.max(...listaNuns)) {  // Atualiza o maior número
+                setResult(numero)
+            } else {
+                setResult(Math.max(...listaNuns))
             }
+        } else if (numero === -1) {
+            setControle(true)
         } else {
-            alert('É necessário que um número inteiro positvo seja digitado')
+            alert('É necessário que um número inteiro positivo seja digitado')
         }
 
+        setInputNumero('')
     }
-
-    
 
     return (
         <div>
@@ -35,20 +34,21 @@ function Numero715() {
             <p>Digite números inteiros e positivos no input abaixo para descobrir qual deles é o maior número</p>
             <p>Obs.: Para encerrar a sequência digitar "-1"</p>
 
-            <input type="number"
+            <input
+                type="number"
                 onChange={(event) => setInputNumero(event.target.value)}
+                value={inputNumero}
             />
-            <button onClick={verificarNumeros}>Verificar</button>
-            {/* <button onClick={aparecerResultado}>Resultado</button> */}
-            <h4>O maior número inteiro positivo é o</h4>
-            {mostrarResultado &&
-                <div>
-                    {verificacao}
-                </div>
-            }
+            <button onClick={verificarNumeros}>Enviar Número</button>
 
+            <h4>O maior número inteiro positivo é:</h4>
+            {controle && (
+                <div>
+                    {result}
+                </div>
+            )}
         </div>
-    )
+    );
 }
 
-export default Numero715
+export default Numero715;
